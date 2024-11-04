213684a9e157fbb6c60969e738b9466e49e69ebf
Joel D. Simard <joel.simard93@gmail.com>
2024-11-04 22:02:27 +0000

    Moved examples

--- examples/example_usage_1.py
+++ examples/example_usage_1.py
@@ -1,6 +1,11 @@
 ### Created by Joel D. Simard
 
 import numpy as np
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
 import linear_daes as ld
 
 
